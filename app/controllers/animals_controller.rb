b053a4419a686077c756986a745937c5e635b2f9
austinb847 <austinb847@icloud.com>
2020-06-12 12:19:57 -0700

    test for get all animals route, and create factorybot file

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -10,18 +10,26 @@ class AnimalsController < ApplicationController
   end
 
   def create
-    @animal = Animal.create(animal_params)
-    json_response(@animal)
+    @animal = Animal.create!(animal_params)
+    json_response(@animal, :created)
   end
 
   def update
     @animal = Animal.find(params[:id])
-    @animal.update(animal_params)
+    if @animal.update!(animal_params)
+      render status: 200, json: {
+        message: "This animal has been updated successfully."
+        }
+    end
   end
 
   def destroy
     @animal = Animal.find(params[:id])
-    @animal.destroy
+    if @animal.destroy!
+      render status: 200, json: {
+        message: "This animal has been deleted successfully."
+        }
+    end
   end
 
   private
