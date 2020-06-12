5218433b6aa826d8eab5d8808e327847ef7bcaee
austinb847 <austinb847@icloud.com>
2020-06-12 11:40:14 -0700

    add controller methods

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,6 +1,33 @@
 class AnimalsController < ApplicationController
   def index
+    @animals = Animal.all
+    json_response(@animals)
   end
 
-  
+  def show
+    @animal = Animal.find(params[:id])
+    json_response(@animal)
+  end
+
+  def create
+    @animal = Animal.create(animal_params)
+    json_response(@animal)
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    @animal.update(animal_params)
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+  end
+
+  private
+  def animal_params
+    params.permit(:species, :breed, :age, :name, :gender)
+  end
+
+
 end
