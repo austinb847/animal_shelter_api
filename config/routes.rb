42186bb128b6fe7416ecd4662046534cb240a3ca
austinb847 <austinb847@icloud.com>
2020-06-12 15:44:53 -0700

    add new route to get random animal, and new controller method for the route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :animals
+  get '/random' => 'animals#random'
 end
