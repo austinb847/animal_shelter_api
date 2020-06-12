e5ddb4c666b17344f8406425fe5f06a6bbab4717
austinb847 <austinb847@icloud.com>
2020-06-12 15:23:05 -0700

    test error header and body for update animal route. test pass

--- spec/requests/put_animal_spec.rb
+++ spec/requests/put_animal_spec.rb
@@ -14,4 +14,17 @@ describe 'Update animal route', :type => :request do
     expect(Animal.find(@animal.id).breed).to eq(@new_breed)
   end
   
+  it 'returns a 404 status not found' do
+    @new_name = 'test'
+    @new_breed = 'test breed'
+    put "/animals/12", params: {name: @new_name, breed: @new_breed}
+    expect(response.status).to eq(404)
+  end
+
+  it 'returns a 404 error message correctly' do
+    @new_name = 'test'
+    @new_breed = 'test breed'
+    put "/animals/12", params: {name: @new_name, breed: @new_breed}
+    expect(JSON.parse(response.body)['message']).to eq("Couldn't find Animal with 'id'=12")
+  end
 end
