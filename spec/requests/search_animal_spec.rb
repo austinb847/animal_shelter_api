a4159354cb5ad48ea903af9f24102afc89fcfa97
austinb847 <austinb847@icloud.com>
2020-06-12 18:59:29 -0700

    test search route. Tests pass

--- spec/requests/search_animal_spec.rb
+++ spec/requests/search_animal_spec.rb
@@ -3,12 +3,14 @@ require 'rails_helper'
 describe "get all search animals route", :type => :request do
 
   before do
-    @animals = FactoryBot.create_list(:animal, 20)
+    @animals = FactoryBot.create_list(:animal, 50)
   end
 
-  it 'returns all dogs ' do
-    get '/search?species=dog'
-    expect(JSON.parse(response.body).size).to eq(20)
+  it 'returns all male animals ' do
+    get '/search?gender=male'
+    # count = JSON.parse(response.body).count{|x| x.gender == "Male"}
+    result = JSON.parse(response.body).map{ |e| e.fetch("gender")}
+    expect(result).to all(include('Male'))
   end
 
   it 'returns status code 200' do
