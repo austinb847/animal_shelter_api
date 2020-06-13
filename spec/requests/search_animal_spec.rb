e3e07cef6a6e2c44af57ae91d1526de9fac251ee
austinb847 <austinb847@icloud.com>
2020-06-13 14:06:53 -0700

    clean up search request test

--- spec/requests/search_animal_spec.rb
+++ spec/requests/search_animal_spec.rb
@@ -4,17 +4,15 @@ describe "get all search animals route", :type => :request do
 
   before do
     @animals = FactoryBot.create_list(:animal, 50)
+    get '/search?gender=male'
   end
 
   it 'returns all male animals ' do
-    get '/search?gender=male'
-    # count = JSON.parse(response.body).count{|x| x.gender == "Male"}
     result = JSON.parse(response.body).map{ |e| e.fetch("gender")}
     expect(result).to all(include('Male'))
   end
 
   it 'returns status code 200' do
-    get '/search?species=dog'
     expect(response).to have_http_status(:success)
   end
 end
